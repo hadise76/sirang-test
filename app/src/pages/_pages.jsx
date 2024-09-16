cef1237d1b31bb7736f0d0649d0237591f5e192b
hadise76 <78503953+hadise76@users.noreply.github.com>
2024-09-16 10:16:15 +0330

    add app file

---
+++ app/src/pages/_pages.jsx
@@ -0,0 +1,4 @@
+// to define the global layout
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
---
+++ app/src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Layout from '../components/layout'
+ 
+export default function Page() {
+  return (
+    /** Your content */
+    <>
+    </>
+  )
+}
+ 
+Page.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
